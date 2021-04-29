cddbb5a4b35e65cb2c3cb1ba6f275b1fae27fd6e
Yutaponta <61647485+Yutaponta@users.noreply.github.com>
2021-04-29 22:25:06 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 //top_view
-var $target = document.querySelector('.target')
-var $button = document.querySelector('.button')
-$button.addEventListener('click', function() {
-  $target.classList.toggle('is-hidden')
-})
+// var $target = document.querySelector('.target')
+// var $button = document.querySelector('.button')
+// $button.addEventListener('click', function() {
+//   $target.classList.toggle('is-hidden')
+// })
 
 //hamburger
 //========================//
